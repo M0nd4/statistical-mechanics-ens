fae2cf773109952d157066c86e1b513c82a7441e
Noel Evans <noelevans@gmail.com>
2014-03-19 21:06:31 +0000

    B2 question done

---
+++ week6/homework_w6_q_b2.py
@@ -0,0 +1,46 @@
+import math, random, pylab
+ 
+def rho_free(x, y, beta):
+    return math.exp(-(x - y) ** 2 / (2.0 * beta))
+ 
+def levy_harmonic_path(xstart, dtau, N):
+    xend = xstart
+    x = [xstart]
+    for k in range(1, N):
+        dtau_prime = (N - k) * dtau
+        Ups1 = 1.0 / math.tanh(dtau) + \
+               1.0 / math.tanh(dtau_prime)
+        Ups2 = x[k - 1] / math.sinh(dtau) + \
+               xend / math.sinh(dtau_prime)
+        x.append(random.gauss(Ups2 / Ups1, \
+               1.0 / math.sqrt(Ups1)))
+    return x
+ 
+beta = 20.0
+N = 80
+dtau = beta / N
+n_steps = 100000
+x = [2.0] * N
+data = []
+acc = 0
+xstart = 0.0
+for step in range(n_steps):
+    x = levy_harmonic_path(xstart, dtau, N)
+    xstart = x[-1]
+    k = random.randint(0, N - 1)
+    data.append(x[k])
+ 
+pylab.hist(data, bins=50, normed=True, label='QMC')
+x_values = [0.1 * a for a in range (-30, 30)]
+y_values = [math.sqrt(math.tanh(beta / 2.0)) / math.sqrt(math.pi) * \
+                math.exp( - xx **2 * math.tanh( beta / 2.0)) for xx in x_values]
+pylab.plot(x_values, y_values, label='exact')
+pylab.xlabel('$x$')
+pylab.ylabel('$\\pi(x)$ (normalized)')
+pylab.axis([-3.0, 3.0, 0.0, 0.6])
+pylab.legend()
+ProgType = 'Levy_harm_path'
+pylab.title(ProgType + ' beta = ' + str(beta) + ', dtau = ' + str(dtau) +
+            ', Nsteps = '+ str(n_steps))
+pylab.savefig(ProgType + str(beta) + '.png')
+pylab.show()
